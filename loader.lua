44db986b67cd1e265ad7fac0d2ce82f1ef86ea7c
WhoIsGenn <85014066+WhoIsGenn@users.noreply.github.com>
2026-01-01 17:45:57 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,11 +1,18 @@
-if not game:IsLoaded() then game.Loaded:Wait() end
+-- Tunggu game load dulu
+if not game:IsLoaded() then 
+    game.Loaded:Wait() 
+end
 task.wait(1.2)
 
+-- Load WindUI dulu
 local WindUI = loadstring(game:HttpGet(
     "https://raw.githubusercontent.com/Footagesus/WindUI/main/main.lua"
 ))()
 
-loadstring(game:HttpGet(
+-- Load Fish-it dan PASS WindUI sebagai parameter
+local FishIt = loadstring(game:HttpGet(
     "https://raw.githubusercontent.com/WhoIsGenn/Fish-it/main/main.lua"
-))()(WindUI)
+))()
 
+-- Panggil FishIt dengan memberikan WindUI
+FishIt(WindUI)
