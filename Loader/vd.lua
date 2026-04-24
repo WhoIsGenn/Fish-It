78f87cfe258d2b4b9fc5f37ddce7e293d385d95b
WhoIsGenn <85014066+WhoIsGenn@users.noreply.github.com>
2026-04-25 05:15:05 +0700

    Update vd.lua

--- Loader/vd.lua
+++ Loader/vd.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/8bb7d73cbab948f7"))()
+loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/0f13052f4299450e"))()
